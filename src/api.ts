76ddf11f698f64f6d5dc14effa9f76260db1e105
Nikhil George <thegeorgenikhil@gmail.com>
2023-01-22 13:20:51 +0530

    update backend api link(migrated to render)

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://quizz-up-backend.up.railway.app/api/",
+  baseURL: "https://quizz-up-backend.onrender.com/api/",
 });
